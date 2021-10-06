40a06bfc9603704b8fc02d79d7d420b7dbb1cbdb
callum.svadkovski <callum.svadkovski@hva.nl>
2021-10-06 22:27:35 +0200

    questions

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,53 +4,62 @@ import {RouterModule, Routes} from '@angular/router';
 import {MainComponent} from './components/views/main/main-layout/main.component';
 import {AuthenticationComponent} from './components/views/authentication/authentication-layout/authentication.component';
 // components
+import {QuestionComponent} from './components/views/main/question/question.component';
 import {HomeComponent} from './components/views/main/home/home.component';
 import {LoginComponent} from './components/views/authentication/login/login.component';
 import {RegisterComponent} from './components/views/authentication/register/register.component';
 import {DashboardComponent} from "./components/views/application/dashboard/dashboard.component";
-import { ContactComponent } from './components/views/main/contact/contact.component';
+import {ContactComponent} from './components/views/main/contact/contact.component';
 import {ApplicationComponent} from "./components/views/application/application-layout/application.component";
 
 
 const routes: Routes = [
-    {
-        path: '',
-        component: MainComponent,
-        children: [
-            {path: '', component: HomeComponent}
-        ]
-    },
-    {
-        path: 'contact',
-        component: MainComponent,
-        children: [
-            {path: '', component: ContactComponent}
-        ]
-    },
-    {
-        path: 'account',
-        component: AuthenticationComponent,
-        children: [
-            {path: '', component: LoginComponent},
-            {path: 'login', component: LoginComponent},
-            {path: 'register', component: RegisterComponent}
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {path: '', component: HomeComponent}
+    ]
+  },
+  {
+    path: 'contact',
+    component: MainComponent,
+    children: [
+      {path: '', component: ContactComponent}
+    ]
+  },
+  {
+    path: 'account',
+    component: AuthenticationComponent,
+    children: [
+      {path: '', component: LoginComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'register', component: RegisterComponent}
 
-        ]
-    },
-    {
-        path: 'dashboard',
-        component: ApplicationComponent,
-        children: [
-            {path: '', component: DashboardComponent},
+    ]
+  },
+  {
+    path: 'dashboard',
+    component: ApplicationComponent,
+    children: [
+      {path: '', component: DashboardComponent},
 
-        ]
-    }
+    ]
+  },
+  {
+    path: 'question',
+    component: QuestionComponent,
+    children: [
+      {path: '', component: QuestionComponent},
+
+    ]
+  }
 ]; // sets up routes constant where you define your routes
 
 // configures NgModule imports and exports
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
