f5e34fe157b26fe09fc9b1760de9a3e7a58a33c9
callum.svadkovski <callum.svadkovski@hva.nl>
2021-10-06 23:50:53 +0200

    survey question pages

--- client/src/app/components/views/application/survey/survey.component.ts
+++ client/src/app/components/views/application/survey/survey.component.ts
@@ -1,18 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-survey',
-    templateUrl: './survey.component.html',
-    styleUrls: ['./survey.component.css']
+  selector: 'app-survey',
+  templateUrl: './survey.component.html',
+  styleUrls: ['./survey.component.css']
 })
 export class SurveyComponent implements OnInit {
 
-    scoringValue: any[number] = {};
+  constructor() { }
 
-    constructor() {
-    }
-
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {
+  }
 
 }
