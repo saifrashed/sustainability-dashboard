211eb7c69607898271bcaf658ec1588786aef0de
Saif Rashed <saif.rashed@hva.nl>
2021-10-06 18:58:05 +0200

    Added basic server

---
+++ server/src/main/java/com/sustainability/sustainabilitydashboard/SustainabilityDashboardApplication.java
@@ -0,0 +1,14 @@
+package com.sustainability.sustainabilitydashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class SustainabilityDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SustainabilityDashboardApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/sustainability/sustainabilitydashboard/SustainabilityDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sustainability.sustainabilitydashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustainabilityDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
