44b57f70dc8272b07ad2fab99d18c03f507b9b79
Saif Rashed <saif.rashed@hva.nl>
2021-10-07 17:54:31 +0200

    edited profile selection

--- client/src/app/_helpers/fake-backend.ts
+++ client/src/app/_helpers/fake-backend.ts
@@ -1,16 +1,23 @@
-import { Injectable } from '@angular/core';
-import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { Observable, of, throwError } from 'rxjs';
-import { delay, mergeMap, materialize, dematerialize } from 'rxjs/operators';
+import {Injectable} from '@angular/core';
+import {
+    HTTP_INTERCEPTORS,
+    HttpEvent,
+    HttpHandler,
+    HttpInterceptor,
+    HttpRequest,
+    HttpResponse
+} from '@angular/common/http';
+import {Observable, of, throwError} from 'rxjs';
+import {delay, dematerialize, materialize, mergeMap} from 'rxjs/operators';
 
-import { User } from '../_models';
+import {User} from '../_models';
 
-const users: User[] = [{ id: 1, username: 'saif', password: 'lars112', firstName: 'Test', lastName: 'User' }];
+const users: User[] = [{id: 1, username: 'saif', password: 'lars112', firstName: 'Test', lastName: 'User'}];
 
 @Injectable()
 export class FakeBackendInterceptor implements HttpInterceptor {
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const { url, method, headers, body } = request;
+        const {url, method, headers, body} = request;
 
         // wrap in delayed observable to simulate server api call
         return of(null)
@@ -34,7 +41,7 @@ export class FakeBackendInterceptor implements HttpInterceptor {
         // route functions
 
         function authenticate() {
-            const { username, password } = body;
+            const {username, password} = body;
             const user = users.find(x => x.username === username && x.password === password);
             if (!user) return error('Username or password is incorrect');
             return ok({
@@ -54,15 +61,15 @@ export class FakeBackendInterceptor implements HttpInterceptor {
         // helper functions
 
         function ok(body: any) {
-            return of(new HttpResponse({ status: 200, body }))
+            return of(new HttpResponse({status: 200, body}))
         }
 
         function error(message: any) {
-            return throwError({ error: { message } });
+            return throwError({error: {message}});
         }
 
         function unauthorized() {
-            return throwError({ status: 401, error: { message: 'Unauthorised' } });
+            return throwError({status: 401, error: {message: 'Unauthorised'}});
         }
 
         function isLoggedIn() {
