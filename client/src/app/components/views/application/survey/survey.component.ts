da6a8467e3567373a39f83230ece0bb7ca6b7453
Saif Rashed <saif.rashed@hva.nl>
2021-10-08 16:49:58 +0200

    fixed ngModel & survey data in TS file

--- client/src/app/components/views/application/survey/survey.component.ts
+++ client/src/app/components/views/application/survey/survey.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-survey',
-  templateUrl: './survey.component.html',
-  styleUrls: ['./survey.component.css']
+    selector: 'app-survey',
+    templateUrl: './survey.component.html',
+    styleUrls: ['./survey.component.css']
 })
 export class SurveyComponent implements OnInit {
 
-  constructor() { }
+    scoringValue: any[number] = {};
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
 
+    ngOnInit(): void {
+    }
 }
