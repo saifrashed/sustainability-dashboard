56ace8a7e8324e88c3626a9ab0dffe2a9ae3be8d
Saif Rashed <saif.rashed@hva.nl>
2021-10-08 13:35:38 +0200

    edited repository

---
+++ server/src/main/java/com/sustainability/sustainabilitydashboard/repositories/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.sustainability.sustainabilitydashboard.repositories;
+
+import com.sustainability.sustainabilitydashboard.models.Todo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends MongoRepository<Todo, String> {
+
+}
