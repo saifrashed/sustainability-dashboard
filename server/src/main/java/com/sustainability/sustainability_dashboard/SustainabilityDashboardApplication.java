09152287212077474d04c26567942a559045bd08
Saif Rashed <saif.rashed@hva.nl>
2021-10-12 01:32:25 +0200

    MongoDB implementation & test files

--- server/src/main/java/com/sustainability/sustainability_dashboard/SustainabilityDashboardApplication.java
+++ server/src/main/java/com/sustainability/sustainability_dashboard/SustainabilityDashboardApplication.java
@@ -1,10 +1,15 @@
 package com.sustainability.sustainability_dashboard;
 
+import com.sustainability.sustainability_dashboard.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SustainabilityDashboardApplication {
+
+	@Autowired
+	ItemRepository groceryItemRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SustainabilityDashboardApplication.class, args);
---
+++ server/src/main/java/com/sustainability/sustainability_dashboard/controller/GroceryController.java
@@ -0,0 +1,29 @@
+package com.sustainability.sustainability_dashboard.controller;
+
+import com.sustainability.sustainability_dashboard.model.GroceryItem;
+import com.sustainability.sustainability_dashboard.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GroceryController {
+
+    @Autowired
+    ItemRepository groceryItemRepo;
+
+    @GetMapping("/add-groceries")
+    public String addGrocery() {
+
+        groceryItemRepo.save(new GroceryItem("Whole Wheat Biscuit", "Whole Wheat Biscuit", 5, "snacks"));
+        groceryItemRepo.save(new GroceryItem("Kodo Millet", "XYZ Kodo Millet healthy", 2, "millets"));
+        groceryItemRepo.save(new GroceryItem("Dried Red Chilli", "Dried Whole Red Chilli", 2, "spices"));
+        groceryItemRepo.save(new GroceryItem("Pearl Millet", "Healthy Pearl Millet", 1, "millets"));
+        groceryItemRepo.save(new GroceryItem("Cheese Crackers", "Bonny Cheese Crackers Plain", 6, "snacks"));
+
+        return "hello";
+    }
+
+
+}
