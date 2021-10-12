e1b1c2f47ee77ddc6c4b05692a778e25f01da84f
Saif Rashed <saif.rashed@hva.nl>
2021-10-12 18:33:54 +0200

    Contains value for scoring inputs

--- client/src/app/components/views/application/survey/survey.component.ts
+++ client/src/app/components/views/application/survey/survey.component.ts
@@ -7,9 +7,12 @@ import {Component, OnInit} from '@angular/core';
 })
 export class SurveyComponent implements OnInit {
 
+    scoringValue: any[number] = {};
+
     constructor() {
     }
 
     ngOnInit(): void {
     }
+
 }
