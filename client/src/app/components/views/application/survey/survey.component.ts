5a522fc835b405900f8de2bd53be0253432b9547
callum.svadkovski <callum.svadkovski@hva.nl>
2021-10-15 16:00:21 +0200

    description/value dynamic

--- client/src/app/components/views/application/survey/survey.component.ts
+++ client/src/app/components/views/application/survey/survey.component.ts
@@ -1,17 +1,42 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnChanges, SimpleChanges, Input} from '@angular/core';
 
 @Component({
-    selector: 'app-survey',
-    templateUrl: './survey.component.html',
-    styleUrls: ['./survey.component.css']
+  selector: 'app-survey',
+  templateUrl: './survey.component.html',
+  styleUrls: ['./survey.component.css']
 })
-export class SurveyComponent implements OnInit {
+export class SurveyComponent implements OnInit, OnChanges {
 
-    scoringValue: any[number] = {};
+  @Input() scoringValue: any[number] = {};
+  @Input() scoringDescription: string[] = ["Adhoc", "Coherent", "Systemic", "Chain-oriented", "Example for others"];
 
-    constructor() {
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+    this.scoringDescriptionString();
+    console.log(this.scoringDescription)
+  }
+
+  scoringDescriptionString() {
+
+    if (this.scoringValue.value == 0) {
+      this.scoringDescription[0]
     }
-
-    ngOnInit(): void {
+    if (this.scoringValue.value == 1) {
+      this.scoringDescription[1]
     }
+    if (this.scoringValue.value == 2) {
+      this.scoringDescription[2]
+    }
+    if (this.scoringValue == 3) {
+      this.scoringDescription[3]
+    }
+    if (this.scoringValue == 4) {
+      this.scoringDescription[4]
+    }
+  }
 }
