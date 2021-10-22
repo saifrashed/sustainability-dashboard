3eded592bb8734d2fb054fa8dd1587af7746f211
Saif Rashed <saif.rashed@hva.nl>
2021-10-22 05:42:03 +0200

    Sort survey list on date

--- client/src/app/components/views/application/faculty/faculty.component.ts
+++ client/src/app/components/views/application/faculty/faculty.component.ts
@@ -1,48 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import {AuthenticationService, SurveyService} from "../../../../_services";
-import {Survey} from "../../../../_models/survey";
-import {Observable} from "rxjs";
-import {SurveyResponseService} from "../../../../_services";
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService, SurveyResponseService, SurveyService} from "../../../../_services";
 
 
 @Component({
-  selector: 'app-faculty',
-  templateUrl: './faculty.component.html',
-  styleUrls: ['./faculty.component.css']
+    selector: 'app-faculty',
+    templateUrl: './faculty.component.html',
+    styleUrls: ['./faculty.component.css']
 })
 export class FacultyComponent implements OnInit {
 
-  public surveyList: any[] = [];
+    public surveyList: any[] = [];
 
-  constructor(private surveyService: SurveyService, private surveyResponse: SurveyResponseService, private authenticationService: AuthenticationService) {
+    constructor(private surveyService: SurveyService, private surveyResponse: SurveyResponseService, private authenticationService: AuthenticationService) {
 
-    this.surveyService.findAll().subscribe(surveyList => {
-      this.updateSurveyList(surveyList);
-    })
-  }
+        this.surveyService.findAll().subscribe(surveyList => {
+            this.actualiseSurveyList(surveyList);
+        })
+    }
 
-  ngOnInit(): void {
-    console.log(this.surveyList)
-  }
+    ngOnInit(): void {
+        console.log(this.surveyList)
+    }
 
 
-  // checks survey list on completion and adds to survey list array
-  updateSurveyList(list: any) {
+    // checks survey list on completion and adds to survey list array
+    actualiseSurveyList(list: any) {
 
-   for(let ndx = 0; ndx < list.length; ndx++) {
-     this.surveyResponse.hasResponded(this.authenticationService.currentUserValue.id, list[ndx].id).subscribe(result => {
+        for (let ndx = 0; ndx < list.length; ndx++) {
+            this.surveyResponse.hasResponded(this.authenticationService.currentUserValue.id, list[ndx].id).subscribe(result => {
 
-       if(result > 0) {
-         list[ndx].status = "Completed";
-       } else {
-         list[ndx].status = "Not Completed";
-       }
+                if (result > 0) {
+                    list[ndx].status = "Completed";
+                } else {
+                    list[ndx].status = "Not Completed";
+                }
 
-       this.surveyList.push(list[ndx]);
+                this.surveyList.push(list[ndx]);
 
-     })
-   }
+                this.surveyList.sort(((a: any, b: any) => {
+                  if ( new Date(parseInt(a.id.substring(0, 8), 16) * 1000) < new Date(parseInt(b.id.substring(0, 8), 16) * 1000) ){
+                    return -1;
+                  }
+                    if ( new Date(parseInt(a.id.substring(0, 8), 16) * 1000) > new Date(parseInt(b.id.substring(0, 8), 16) * 1000) ){
+                    return 1;
+                  }
+                  return 0;
+                }))
 
-  }
+            })
+        }
+
+    }
 
 }
