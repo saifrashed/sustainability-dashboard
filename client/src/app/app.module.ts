29884595bb8bb4145f561a06ef905b89ef19300e
callum.svadkovski <callum.svadkovski@hva.nl>
2021-11-13 20:11:27 +0100

    admin graph view

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,7 +3,9 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module'; // CLI imports AppRoutingModule
 import {HttpClientModule} from '@angular/common/http';
-import { NotifierModule, NotifierOptions } from 'angular-notifier';
+import {NotifierModule, NotifierOptions} from 'angular-notifier';
+import {NgxEchartsModule} from 'ngx-echarts';
+
 
 // layouts
 import {MainComponent} from './components/views/main/main-layout/main.component';
@@ -21,79 +23,81 @@ import {SurveyComponent} from "./components/views/application/survey/survey.comp
 import {SurveyQuestionComponent} from './components/views/application/survey-question/survey-question.component';
 
 
-
 /**
  * Custom angular notifier options
  */
 const customNotifierOptions: NotifierOptions = {
-    position: {
-        horizontal: {
-            position: 'right',
-            distance: 12
-        },
-        vertical: {
-            position: 'bottom',
-            distance: 12,
-            gap: 10
-        }
+  position: {
+    horizontal: {
+      position: 'right',
+      distance: 12
     },
-    theme: 'material',
-    behaviour: {
-        autoHide: 5000,
-        onClick: 'hide',
-        onMouseover: 'pauseAutoHide',
-        showDismissButton: true,
-        stacking: 4
-    },
-    animations: {
-        enabled: true,
-        show: {
-            preset: 'slide',
-            speed: 300,
-            easing: 'ease'
-        },
-        hide: {
-            preset: 'fade',
-            speed: 300,
-            easing: 'ease',
-            offset: 50
-        },
-        shift: {
-            speed: 300,
-            easing: 'ease'
-        },
-        overlap: 150
+    vertical: {
+      position: 'bottom',
+      distance: 12,
+      gap: 10
     }
+  },
+  theme: 'material',
+  behaviour: {
+    autoHide: 5000,
+    onClick: 'hide',
+    onMouseover: 'pauseAutoHide',
+    showDismissButton: true,
+    stacking: 4
+  },
+  animations: {
+    enabled: true,
+    show: {
+      preset: 'slide',
+      speed: 300,
+      easing: 'ease'
+    },
+    hide: {
+      preset: 'fade',
+      speed: 300,
+      easing: 'ease',
+      offset: 50
+    },
+    shift: {
+      speed: 300,
+      easing: 'ease'
+    },
+    overlap: 150
+  }
 };
 
 @NgModule({
-    declarations: [
-        MainComponent,
-        AuthenticationComponent,
-        ApplicationComponent,
-        AppComponent,
-        HomeComponent,
-        MainComponent,
-        AuthenticationComponent,
-        ApplicationComponent,
-        LoginComponent,
-        RegisterComponent,
-        AdminComponent,
-        ContactComponent,
-        FacultyComponent,
-        SurveyComponent,
-        SurveyQuestionComponent,
-    ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        AppRoutingModule,
-        HttpClientModule,
-        ReactiveFormsModule,
-        NotifierModule.withConfig(customNotifierOptions)
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    MainComponent,
+    AuthenticationComponent,
+    ApplicationComponent,
+    AppComponent,
+    HomeComponent,
+    MainComponent,
+    AuthenticationComponent,
+    ApplicationComponent,
+    LoginComponent,
+    RegisterComponent,
+    AdminComponent,
+    ContactComponent,
+    FacultyComponent,
+    SurveyComponent,
+    SurveyQuestionComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    NotifierModule.withConfig(customNotifierOptions),
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts')
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
--- client/src/app/components/views/application/admin/admin.component.ts
+++ client/src/app/components/views/application/admin/admin.component.ts
@@ -3,232 +3,282 @@ import {FormControl, FormGroup} from '@angular/forms';
 import {AuthenticationService, SurveyResponseService, SurveyService} from "../../../../_services";
 import {Survey} from "../../../../_models/survey";
 import {NotifierService} from "angular-notifier";
+import {EChartsOption} from 'echarts';
 
 @Component({
-    selector: 'app-dashboard-admin',
-    templateUrl: './admin.component.html',
-    styleUrls: ['./admin.component.css']
+  selector: 'app-dashboard-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
 
-    public surveyList: any;
-    public userList: any;
+  public surveyList: any;
+  public userList: any;
 
-    public selectedSurveyQuestions: any = [];
-    public selectedSurveyId: any;
+  public selectedSurveyQuestions: any = [];
+  public selectedSurveyId: any;
 
-    public facultyList: any;
+  public facultyList: any;
 
 
-    public newUserForm = new FormGroup({
-        username: new FormControl(''),
-        email: new FormControl(''),
-        faculty: new FormControl(''),
-        password: new FormControl(''),
-        role: new FormControl(['ROLE_FACULTY']),
+  public newUserForm = new FormGroup({
+    username: new FormControl(''),
+    email: new FormControl(''),
+    faculty: new FormControl(''),
+    password: new FormControl(''),
+    role: new FormControl(['ROLE_FACULTY']),
+  });
+
+  public newSurveyForm = new FormGroup({
+    title: new FormControl(''),
+    pillar: new FormControl(''),
+    optionOne: new FormControl('Bad'),
+    optionTwo: new FormControl('Below average'),
+    optionThree: new FormControl('Average'),
+    optionFour: new FormControl('Above average'),
+    optionFive: new FormControl('Good'),
+  });
+
+
+  public newQuestionForm = new FormGroup({
+    description: new FormControl(''),
+    weight: new FormControl(1),
+  });
+
+
+  // statistics
+
+  public globalStatistics: any;
+  public facultyStatistics: any;
+
+  constructor(
+    private surveyService: SurveyService,
+    private authenticationService: AuthenticationService,
+    private surveyResponse: SurveyResponseService,
+    private notifierService: NotifierService
+  ) {
+
+    this.surveyService.findAll().subscribe(surveyList => {
+      this.surveyList = surveyList;
+      this.getCompletedSurveys();
+
+      //
+      this.notifierService.notify("success", "All surveys have been loaded", "GET_SURVEY_SUCCESS")
     });
 
-    public newSurveyForm = new FormGroup({
-        title: new FormControl(''),
-        pillar: new FormControl(''),
-        optionOne: new FormControl('Bad'),
-        optionTwo: new FormControl('Below average'),
-        optionThree: new FormControl('Average'),
-        optionFour: new FormControl('Above average'),
-        optionFive: new FormControl('Good'),
+    this.authenticationService.findAll().subscribe(userList => {
+      this.userList = userList;
     });
 
-
-    public newQuestionForm = new FormGroup({
-        description: new FormControl(''),
-        weight: new FormControl(1),
+    this.surveyResponse.findAllFaculties().subscribe(faculties => {
+      this.facultyList = faculties;
     });
 
+    this.surveyResponse.getSurveyResponseStatisticsGlobals().subscribe(statistics => {
+      this.globalStatistics = statistics;
+    });
+  }
 
-    // statistics
+  ngOnInit(): void {
+    this.notifierService.notify("success", "Welcome to the Dashboard " + this.authenticationService.currentUserValue.username, "WELCOME_MESSAGE")
+  }
 
-    public globalStatistics: any;
-    public facultyStatistics: any;
+  getUsers() {
+    this.authenticationService.findAll().subscribe(userList => {
+      this.userList = userList;
+    })
+  }
 
-    constructor(
-        private surveyService: SurveyService,
-        private authenticationService: AuthenticationService,
-        private surveyResponse: SurveyResponseService,
-        private notifierService: NotifierService
-    ) {
+  deleteUser(id: string) {
+    if (confirm("Are you sure?")) {
+      this.authenticationService.deleteById(id).subscribe(message => {
+        this.getUsers();
+        this.notifierService.notify("success", "User successfully deleted. ", "SUCCESS_USERDELETE")
+      })
+    } else {
+      this.notifierService.notify("error", "User not deleted. ", "FAIL_USERDELETE")
+    }
+  }
 
-        this.surveyService.findAll().subscribe(surveyList => {
-            this.surveyList = surveyList;
-            this.getCompletedSurveys();
+  addUser() {
+    this.authenticationService.create(this.newUserForm.getRawValue()).subscribe(message => {
+      this.getUsers();
+      this.newUserForm.reset();
+    }, error => {
+      for (let i = 0; i < error.error.errors.length; i++) {
+        this.notifierService.notify('error', error.error.errors[i].field + ": " + error.error.errors[i].defaultMessage, 'LOGIN_ERROR');
+      }
+    })
+  }
 
-            //
-            this.notifierService.notify("success", "All surveys have been loaded", "GET_SURVEY_SUCCESS")
-        });
+  responsesFilledError() {
+    this.notifierService.notify("error", "Cannot edit questions because of filled responses", 'SURVEY_RESPONSE_ERROR')
+  }
 
-        this.authenticationService.findAll().subscribe(userList => {
-            this.userList = userList;
-        });
+  // Survey CRUD
+  getSurveys() {
+    this.surveyService.findAll().subscribe(surveyList => {
+      this.surveyList = surveyList;
+      this.notifierService.notify("success", "All surveys have been loaded", "GET_SURVEY_SUCCESS")
+    });
+  }
 
-        this.surveyResponse.findAllFaculties().subscribe(faculties => {
-            this.facultyList = faculties;
-        });
+  getCompletedSurveys() {
+    for (let index in this.surveyList) {
+      this.surveyResponse.getCompletedSurveys(this.surveyList[index].id).subscribe(result => {
+        this.surveyList[index].completed = result;
+      });
+    }
+  }
 
-        this.surveyResponse.getSurveyResponseStatisticsGlobals().subscribe(statistics => {
-            this.globalStatistics = statistics;
-        });
+
+  getSurveyQuestions(id: string) {
+    this.surveyService.findAllQuestions(id).subscribe(surveyQuestions => {
+      this.selectedSurveyQuestions = surveyQuestions;
+      this.selectedSurveyId = id;
+    })
+  }
+
+  chartOption: EChartsOption = {
+    xAxis: {
+      type: 'category',
+      data: ['June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    },
+    yAxis: {
+      type: 'value',
+      name: 'Surveys',
+      min: 0,
+      max: 100,
+      position: 'left'
+    },
+    series: [
+      {
+        name: 'X-1',
+        type: 'line',
+        stack: 'counts',
+        data: [2, 5, 12, 23, 25, 29, 45]
+      },
+      {
+        name: 'X-2',
+        type: 'line',
+        stack: 'counts',
+        data: [6, 3, 20, 44, 53, 34, 44]
+      },
+      {
+        name: 'X-3',
+        type: 'line',
+        stack: 'counts',
+        data: [15, 15, 39, 66, 76, 82, 70]
+      },
+      {
+        name: 'X-4',
+        type: 'line',
+        stack: 'counts',
+        data: [1, 6, 15, 32, 31, 53, 30]
+      }]
+  };
+
+  addSurvey() {
+
+    let surveyObject: Survey = {
+      id: null,
+      title: this.newSurveyForm.controls["title"].value,
+      pillar: this.newSurveyForm.controls["pillar"].value,
+      scoringDescription: [
+        this.newSurveyForm.controls["optionOne"].value,
+        this.newSurveyForm.controls["optionTwo"].value,
+        this.newSurveyForm.controls["optionThree"].value,
+        this.newSurveyForm.controls["optionFour"].value,
+        this.newSurveyForm.controls["optionFive"].value
+      ]
+    };
+
+
+    this.surveyService.create(surveyObject).subscribe(message => {
+      this.getSurveys();
+      this.newSurveyForm.reset();
+      this.newSurveyForm.controls["optionOne"].setValue("Bad");
+      this.newSurveyForm.controls["optionTwo"].setValue("Below average");
+      this.newSurveyForm.controls["optionThree"].setValue("Average");
+      this.newSurveyForm.controls["optionFour"].setValue("Above average");
+      this.newSurveyForm.controls["optionFive"].setValue("Good");
+    });
+  }
+
+
+  addSurveyQuestion() {
+
+    if (!this.newQuestionForm.controls["description"].value || !this.newQuestionForm.controls["weight"].value) {
+      this.notifierService.notify("error", "Fill in all fields", 'SURVEY_ADD_ERROR');
+      return;
     }
 
-    ngOnInit(): void {
-        this.notifierService.notify("success", "Welcome to the Dashboard " + this.authenticationService.currentUserValue.username, "WELCOME_MESSAGE")
+    let questionObject = {
+      id: null,
+      surveyId: this.selectedSurveyId,
+      description: this.newQuestionForm.controls["description"].value,
+      weight: this.newQuestionForm.controls["weight"].value,
+    };
+
+    this.surveyService.createQuestion(questionObject).subscribe(message => {
+      this.getSurveyQuestions(this.selectedSurveyId);
+      this.newQuestionForm.reset();
+      this.newQuestionForm.controls["weight"].setValue(1);
+      this.notifierService.notify("success", "Question has been added to the survey", "ADD_SURVEY_QUESTION")
+    })
+  }
+
+// SurveyResponse model
+
+  getFaculties() {
+    this.surveyResponse.findAllFaculties().subscribe(faculties => {
+      this.facultyList = faculties;
+    })
+  }
+
+  getFacultyAverages(faculty
+                       :
+                       string
+  ) {
+    this.surveyResponse.getSurveyResponseStatisticsByFaculty(faculty).subscribe(statistics => {
+      this.facultyStatistics = statistics;
+    })
+  }
+
+  getGlobalAverage(pillar
+                     :
+                     string
+  ):
+    number {
+    if (this.globalStatistics) {
+      let object = this.globalStatistics.find((x: any) => x._id == pillar);
+
+      if (object == undefined) {
+        return 0;
+      }
+
+      return object.average.toFixed(1);
+    } else {
+      return 0;
     }
+  }
 
-    getUsers() {
-        this.authenticationService.findAll().subscribe(userList => {
-            this.userList = userList;
-        })
+  getFacultyAverage(pillar
+                      :
+                      string
+  ):
+    number {
+    if (this.facultyStatistics) {
+      let object = this.facultyStatistics.find((x: any) => x._id == pillar);
+
+      if (object == undefined) {
+        return 0;
+      }
+
+      return object.average.toFixed(1);
+    } else {
+      return 0;
     }
-
-    deleteUser(id: string) {
-        if (confirm("Are you sure?")) {
-            this.authenticationService.deleteById(id).subscribe(message => {
-                this.getUsers();
-                this.notifierService.notify("success", "User successfully deleted. ", "SUCCESS_USERDELETE")
-            })
-        } else {
-            this.notifierService.notify("error", "User not deleted. ", "FAIL_USERDELETE")
-        }
-    }
-
-    addUser() {
-        this.authenticationService.create(this.newUserForm.getRawValue()).subscribe(message => {
-            this.getUsers();
-            this.newUserForm.reset();
-        }, error => {
-            for (let i = 0; i < error.error.errors.length; i++) {
-                this.notifierService.notify('error', error.error.errors[i].field + ": " + error.error.errors[i].defaultMessage, 'LOGIN_ERROR');
-            }
-        })
-    }
-
-    responsesFilledError() {
-        this.notifierService.notify("error", "Cannot edit questions because of filled responses", 'SURVEY_RESPONSE_ERROR')
-    }
-
-    // Survey CRUD
-    getSurveys() {
-        this.surveyService.findAll().subscribe(surveyList => {
-            this.surveyList = surveyList;
-            this.notifierService.notify("success", "All surveys have been loaded", "GET_SURVEY_SUCCESS")
-        });
-    }
-
-    getCompletedSurveys() {
-        for (let index in this.surveyList) {
-            this.surveyResponse.getCompletedSurveys(this.surveyList[index].id).subscribe(result => {
-                this.surveyList[index].completed = result;
-            });
-        }
-    }
-
-
-    getSurveyQuestions(id: string) {
-        this.surveyService.findAllQuestions(id).subscribe(surveyQuestions => {
-            this.selectedSurveyQuestions = surveyQuestions;
-            this.selectedSurveyId = id;
-        })
-    }
-
-
-    addSurvey() {
-
-        let surveyObject: Survey = {
-            id: null,
-            title: this.newSurveyForm.controls["title"].value,
-            pillar: this.newSurveyForm.controls["pillar"].value,
-            scoringDescription: [
-                this.newSurveyForm.controls["optionOne"].value,
-                this.newSurveyForm.controls["optionTwo"].value,
-                this.newSurveyForm.controls["optionThree"].value,
-                this.newSurveyForm.controls["optionFour"].value,
-                this.newSurveyForm.controls["optionFive"].value
-            ]
-        };
-
-
-        this.surveyService.create(surveyObject).subscribe(message => {
-            this.getSurveys();
-            this.newSurveyForm.reset();
-            this.newSurveyForm.controls["optionOne"].setValue("Bad");
-            this.newSurveyForm.controls["optionTwo"].setValue("Below average");
-            this.newSurveyForm.controls["optionThree"].setValue("Average");
-            this.newSurveyForm.controls["optionFour"].setValue("Above average");
-            this.newSurveyForm.controls["optionFive"].setValue("Good");
-        });
-    }
-
-
-    addSurveyQuestion() {
-
-        if (!this.newQuestionForm.controls["description"].value || !this.newQuestionForm.controls["weight"].value) {
-            this.notifierService.notify("error", "Fill in all fields", 'SURVEY_ADD_ERROR');
-            return;
-        }
-
-        let questionObject = {
-            id: null,
-            surveyId: this.selectedSurveyId,
-            description: this.newQuestionForm.controls["description"].value,
-            weight: this.newQuestionForm.controls["weight"].value,
-        };
-
-        this.surveyService.createQuestion(questionObject).subscribe(message => {
-            this.getSurveyQuestions(this.selectedSurveyId);
-            this.newQuestionForm.reset();
-            this.newQuestionForm.controls["weight"].setValue(1);
-            this.notifierService.notify("success", "Question has been added to the survey", "ADD_SURVEY_QUESTION")
-        })
-    }
-
-    // SurveyResponse model
-
-    getFaculties() {
-        this.surveyResponse.findAllFaculties().subscribe(faculties => {
-            this.facultyList = faculties;
-        })
-    }
-
-    getFacultyAverages(faculty: string) {
-        this.surveyResponse.getSurveyResponseStatisticsByFaculty(faculty).subscribe(statistics => {
-            this.facultyStatistics = statistics;
-        })
-    }
-
-    getGlobalAverage(pillar: string): number {
-        if (this.globalStatistics) {
-            let object = this.globalStatistics.find((x: any) => x._id == pillar);
-
-            if (object == undefined) {
-                return 0;
-            }
-
-            return object.average.toFixed(1);
-        } else {
-            return 0;
-        }
-    }
-
-    getFacultyAverage(pillar: string): number {
-        if (this.facultyStatistics) {
-            let object = this.facultyStatistics.find((x: any) => x._id == pillar);
-
-            if (object == undefined) {
-                return 0;
-            }
-
-            return object.average.toFixed(1);
-        } else {
-            return 0;
-        }
-    }
+  }
 
 }
